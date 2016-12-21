30328648fb86f9d04da80566f3d8e77328033001
Michael West <westenterprisetechnologies001@gmail.com>
2016-12-21 10:45:01 -0600

    2016122110 - Re-architected the persistence unit in order to allow multiple modules to provide their own entity managers and corresponding datasources

--- common-dao/src/main/java/com/wet/api/common/dao/impl/AbstractDaoJdbc.java
+++ common-dao/src/main/java/com/wet/api/common/dao/impl/AbstractDaoJdbc.java
@@ -4,17 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.wet.api.common.model.DomainEntity;
 
 public abstract class AbstractDaoJdbc<T extends DomainEntity> extends AbstractDao<T>
-{
-	@Autowired
-	protected DataSource dataSource;
-	
+{	
 	public AbstractDaoJdbc(Class<T> type)
 	{
 		super(type);
